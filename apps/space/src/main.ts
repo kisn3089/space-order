079a52f41e362eb9e07de0956c2a48e4d8ec71bb
Stems <kisn3089@naver.com>
2025-11-18 17:21:23 +0900

    space change: root -> app

--- apps/space/src/main.ts
+++ apps/space/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AppModule } from './root/app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

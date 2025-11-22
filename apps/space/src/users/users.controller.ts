74e70b87c8f7c674f2a5d61716a408f28997d784
Insung Hwang <kisn3089@naver.com>
2025-11-22 19:55:09 +0900

    Feat: downgraded Prisma at 7 to 6 (#1)
    * backup: prisma v7
    
    * feat: downgrade 설정에 맞게 docker 설정 변경
    - volume에서 dist 폴더 삭제
    
    * feat: prisma downgrade 맞게 설정 변경 및 DB migration 진행
    
    * fix: nest-cli.json deleteOutDir 설정 true로 rollback
    
    * fix: [Review] class-validator IsString -> IsEmail

--- apps/space/src/users/users.controller.ts
+++ apps/space/src/users/users.controller.ts
@@ -1,50 +1,42 @@
 import {
-  Body,
   Controller,
   Get,
-  Param,
   Post,
-  HttpCode,
+  Body,
   Patch,
+  Param,
   Delete,
-  Query,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get() // GET /users or /users?role=value
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return this.usersService.findAll(role);
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
-  @Get('me')
-  findMe() {
-    return this.usersService.findMe();
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
   }
 
-  @Get(':id') // GET /users/:id
+  @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+    return this.usersService.findOne(+id);
   }
 
-  @Post() // POST /users
-  @HttpCode(207)
-  create(@Body() user: CreateUserDto) {
-    return this.usersService.create(user);
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
   }
 
-  @Patch(':id') // PATCH /users/:id
-  @HttpCode(209)
-  update(@Param('id') id: string, @Body() userUpdate: Partial<CreateUserDto>) {
-    return this.usersService.update(id, userUpdate);
-  }
-
-  @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return this.usersService.delete(id);
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }

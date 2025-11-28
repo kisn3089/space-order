b90832886b7ba9ef4e498c3bbfdf7d0bdcc78deb
Stems <kisn3089@naver.com>
2025-11-28 19:43:24 +0900

    orderdesk/feat: shadcn (sidebar) 생성

---
+++ apps/orderdesk/src/app/(sidebar)/layout.tsx
@@ -0,0 +1,24 @@
+import {
+  SidebarProvider,
+  SidebarTrigger,
+} from "@spaceorder/ui/components/sidebar";
+import { NavSidebar } from "./page";
+
+export default function SidebarLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <section className="antialiased">
+      <SidebarProvider>
+        <NavSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+      {children}
+    </section>
+  );
+}
---
+++ apps/orderdesk/src/app/(sidebar)/test/layout.tsx
@@ -0,0 +1,3 @@
+export default function TestPage({ children }: { children: React.ReactNode }) {
+  return <div className="w-screen h-screen">{children}</div>;
+}

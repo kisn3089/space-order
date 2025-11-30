afbee38dc1e614f2a2b111c400c8bbef25d3ff99
Stems <kisn3089@naver.com>
2025-12-01 01:54:40 +0900

    orderhub/feat: localhost 통신 가능하도록 CORS 설정

--- apps/orderhub/src/main.ts
+++ apps/orderhub/src/main.ts
@@ -11,6 +11,12 @@ BigInt.prototype.toJSON = function () {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    // allowedHeaders: 'Content-Type, Authorization',
+  });
 
   // Enable validation globally
   app.useGlobalPipes(

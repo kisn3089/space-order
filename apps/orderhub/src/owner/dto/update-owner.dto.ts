a4620ef219850ed29912acdd5ee4500a1352abdf
Stems <kisn3089@naver.com>
2025-12-26 23:52:08 +0900

    orderhub/refect: owner dto zod로 통일 불필요한 dto 삭제

--- apps/orderhub/src/owner/dto/update-owner.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { OwnerResponseDto } from './response-owner.dto';
-
-// [TODO:] 테스트 필요
-export class UpdateOwnerDto extends PartialType(
-  OmitType(OwnerResponseDto, ['password', 'email'] as const),
-) {}

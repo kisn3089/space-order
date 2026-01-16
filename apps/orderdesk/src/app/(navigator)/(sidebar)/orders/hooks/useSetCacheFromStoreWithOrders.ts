978112c4c8a5d0994a156bd7c96b2fb5cb4517ad
Stems <kisn3089@naver.com>
2026-01-16 17:53:52 +0900

    orderdesk/feat: 주문 관리 페이지에 필요한 데이터 bulk 호출하여 리소스 계층화하여 캐시 저장

---
+++ apps/orderdesk/src/app/(navigator)/(sidebar)/orders/hooks/useSetCacheFromStoreWithOrders.ts
@@ -0,0 +1,64 @@
+import {
+  ResponseOrderWithItem,
+  ResponseStore,
+  ResponseStoreWithTables,
+  ResponseTableWithSessions,
+  TABLE_QUERY_FILTER_CONST,
+  TABLE_QUERY_INCLUDE_CONST,
+} from "@spaceorder/db";
+import { useQueryClient } from "@tanstack/react-query";
+
+export const useSetCacheFromStoreWithOrders = () => {
+  const queryClient = useQueryClient();
+
+  const setData = (storeWithTables: ResponseStoreWithTables) => {
+    const { tables, ...store } = storeWithTables;
+    const { ALIVE_SESSION } = TABLE_QUERY_FILTER_CONST;
+    const { ORDERS } = TABLE_QUERY_INCLUDE_CONST;
+
+    queryClient.setQueryData<ResponseStore>(["/stores"], store);
+
+    const tablesWithAliveSessions = tables.map((tableWithSessions) => {
+      const { tableSessions, ...table } = tableWithSessions;
+
+      queryClient.setQueryData<ResponseTableWithSessions>(
+        [
+          `/stores/${store.publicId}/tables/${table.publicId}?include=${ORDERS}&filter=${ALIVE_SESSION}`,
+        ],
+        tableWithSessions
+      );
+
+      if (tableSessions && tableSessions[0]) {
+        /**
+         * TODO: orders의 기본 조회가 모든 orders로 변경되고 query(alive)에 따라 활성화된 테이블 세션의 orders를 조회한다면
+         * 캐시 url 설정도 변경 필요 &filter=alive 같은 식으로
+         */
+
+        queryClient.setQueryData<ResponseOrderWithItem[]>(
+          [`/owner/stores/${store.publicId}/tables/${table.publicId}/orders`],
+          tableSessions[0].orders
+        );
+
+        tableSessions[0].orders?.forEach((order) => {
+          queryClient.setQueryData<ResponseOrderWithItem>(
+            [
+              `/owner/stores/${store.publicId}/tables/${table.publicId}/orders/${order.publicId}`,
+            ],
+            order
+          );
+        });
+      }
+
+      return tableWithSessions;
+    });
+
+    queryClient.setQueryData<ResponseTableWithSessions[]>(
+      [
+        `/stores/${store.publicId}/tables?include=${ORDERS}&filter=${ALIVE_SESSION}`,
+      ],
+      tablesWithAliveSessions
+    );
+  };
+
+  return setData;
+};

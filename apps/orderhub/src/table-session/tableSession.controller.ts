73e9d4ae26bec19b4550c4dc4a8a5d046c6abbec
Stems <kisn3089@naver.com>
2026-01-21 19:26:56 +0900

    orderhub/feat: tableSession 엔터티 query params 지원하도록 구현

--- apps/orderhub/src/table-session/tableSession.controller.ts
+++ apps/orderhub/src/table-session/tableSession.controller.ts
@@ -10,31 +10,52 @@ import {
   Patch,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Query,
 } from '@nestjs/common';
 import { TableSessionService } from './tableSession.service';
 import {
+  sessionParamsSchema,
+  sessionListQuerySchema,
   tableParamsSchema,
   updateSessionSchema,
+  sessionUniqueQuerySchema,
 } from '@spaceorder/api/schemas';
 import { ZodValidation } from 'src/utils/guards/zod-validation.guard';
 import { responseCookie } from 'src/utils/cookies';
 import { COOKIE_TABLE } from '@spaceorder/db/constants';
-import type { ResponseTableSession, TableSession } from '@spaceorder/db';
+import {
+  TableSessionStatus,
+  type ResponseTableSession,
+  type TableSession,
+} from '@spaceorder/db';
 import { SessionAuth } from 'src/utils/guards/table-session-auth.guard';
 import { Session } from 'src/decorators/tableSession.decorator';
 import type { z } from 'zod';
 import { TableSessionResponseDto } from './dto/tableSessionResponse.dto';
 import { SessionPermission } from 'src/utils/guards/model-permissions/table-session-permission.guard';
+import { JwtAuthGuard } from 'src/utils/guards/jwt-auth.guard';
+import {
+  SESSION_FILTER_RECORD,
+  SESSION_INCLUDE_KEY_RECORD,
+} from './table-session-query.const';
+import { BuildIncludeService } from 'src/utils/query-params/build-include';
 
 export type UpdateTableSessionDto = z.infer<typeof updateSessionSchema>;
+type SessionQueryParams = {
+  filter?: keyof typeof SESSION_FILTER_RECORD;
+  include?: keyof typeof SESSION_INCLUDE_KEY_RECORD;
+};
 
-@Controller('tables/:tableId/session')
-@UseGuards(ZodValidation({ params: tableParamsSchema }))
+@Controller('tables/:tableId/sessions')
 @UseInterceptors(ClassSerializerInterceptor)
 export class TableSessionController {
-  constructor(private readonly tableSessionService: TableSessionService) {}
+  constructor(
+    private readonly tableSessionService: TableSessionService,
+    private readonly buildInclude: BuildIncludeService,
+  ) {}
 
   @Post()
+  @UseGuards(ZodValidation({ params: tableParamsSchema }))
   async findActivatedSessionOrCreate(
     @Param('tableId') tablePublicId: string,
     @Res({ passthrough: true }) response: Response,
@@ -54,10 +75,59 @@ export class TableSessionController {
     return new TableSessionResponseDto(findOrCreatedSession);
   }
 
+  /** 세션 조회는 관리자만 수행한다. */
+  @Get()
+  @UseGuards(
+    JwtAuthGuard,
+    ZodValidation({
+      params: tableParamsSchema,
+      query: sessionListQuerySchema,
+    }),
+  )
+  async getList(
+    @Param('tableId') tablePublicId: string,
+    @Query() query?: SessionQueryParams,
+  ): Promise<ResponseTableSession[]> {
+    const { include, filter } = this.buildInclude.build({
+      query,
+      includeKeyRecord: SESSION_INCLUDE_KEY_RECORD,
+      filterRecord: SESSION_FILTER_RECORD,
+    });
+
+    return await this.tableSessionService.getSessionList({
+      where: { table: { publicId: tablePublicId }, ...filter },
+      ...include,
+    });
+  }
+
+  @Get(':sessionId')
+  @UseGuards(
+    JwtAuthGuard,
+    ZodValidation({
+      params: sessionParamsSchema,
+      query: sessionUniqueQuerySchema,
+    }),
+  )
+  async getUnique(
+    @Param('sessionId') sessionId: string,
+    @Query() query?: SessionQueryParams,
+  ): Promise<ResponseTableSession> {
+    const { include } = this.buildInclude.build({
+      query,
+      includeKeyRecord: SESSION_INCLUDE_KEY_RECORD,
+      filterRecord: SESSION_FILTER_RECORD,
+    });
+
+    return await this.tableSessionService.getSessionUnique({
+      where: { publicId: sessionId },
+      ...include,
+    });
+  }
+
   @Patch()
   @UseGuards(
     SessionAuth,
-    ZodValidation({ body: updateSessionSchema }),
+    ZodValidation({ params: tableParamsSchema, body: updateSessionSchema }),
     SessionPermission,
   )
   async partialUpdate(
@@ -80,24 +150,5 @@ export class TableSessionController {
     }
 
     return updatedSession;
-  }
-
-  @Get()
-  /** TODO: 전체 세션 정보를 볼 필요가 있을까? (개발 이후에 삭제 고려) */
-  async getList(
-    @Param('tableId') tablePublicId: string,
-  ): Promise<ResponseTableSession[]> {
-    return await this.tableSessionService.getSessionList(tablePublicId);
-  }
-
-  /** TODO: 테스트를 위한 임시 */
-  @Get(':sessionPublicId')
-  async getSessionBySessionToken(
-    @Param('sessionPublicId') sessionPublicId: string,
-  ): Promise<TableSessionResponseDto> {
-    const findTableSession =
-      await this.tableSessionService.getSessionByTableId(sessionPublicId);
-
-    return new TableSessionResponseDto(findTableSession);
   }
 }
--- apps/orderhub/src/table-session/tableSession.module.ts
+++ apps/orderhub/src/table-session/tableSession.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TableSessionService } from './tableSession.service';
 import { TableSessionController } from './tableSession.controller';
+import { BuildIncludeService } from 'src/utils/query-params/build-include';
 
 @Module({
   controllers: [TableSessionController],
-  providers: [TableSessionService],
+  providers: [TableSessionService, BuildIncludeService],
   exports: [TableSessionService],
 })
 export class TableSessionModule {}

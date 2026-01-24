f1431a04eb214c9b867104dc12450850b55d5c51
Stems <kisn3089@naver.com>
2026-01-24 23:20:53 +0900

    orderdesk/ui: 주문 상세 영역에서 주문 선택 시 변경 버튼 추가

--- apps/orderdesk/src/app/(navigator)/(sidebar)/stores/[storeId]/orders/components/table-order-detail/order-table/EditController.tsx
+++ apps/orderdesk/src/app/(navigator)/(sidebar)/stores/[storeId]/orders/components/table-order-detail/order-table/EditController.tsx
@@ -1,6 +1,11 @@
 import { TableCell } from "@spaceorder/ui/components/table";
 import { columns } from "../columns";
 import { Button } from "@spaceorder/ui/components/button";
+import { AlertDialogWrapper } from "@spaceorder/ui/components/alert-dialog/AlertDialogWrapper";
+import { AlertDialogAction } from "@spaceorder/ui/components/alert-dialog/alert-dialog";
+
+const dialogTitle = "해당 기능은 준비 중입니다.";
+const dialogDescription = "업데이트를 기다려주세요!";
 
 type EditControllerProps = {
   setChanges: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -12,11 +17,32 @@ export default function EditController({
 }: EditControllerProps) {
   return (
     <TableCell colSpan={columns.length} className="p-0">
-      <div className="w-full grid grid-cols-[1fr_2fr] gap-2 p-2">
-        <Button variant={"destructive"} onClick={remove}>
+      <div className="w-full grid grid-cols-[1fr_2fr_3fr] gap-2 p-2">
+        <Button
+          className="font-semibold"
+          variant={"destructive"}
+          onClick={remove}
+        >
           삭제
         </Button>
-        <Button variant={"default"} onClick={setChanges}>
+        <AlertDialogWrapper
+          title={dialogTitle}
+          description={dialogDescription}
+          renderFooter={() => <AlertDialogAction>확인</AlertDialogAction>}
+        >
+          <Button
+            className="font-semibold"
+            variant={"outline"}
+            onClick={(e) => e.stopPropagation()}
+          >
+            메뉴 변경
+          </Button>
+        </AlertDialogWrapper>
+        <Button
+          className="font-semibold"
+          variant={"default"}
+          onClick={setChanges}
+        >
           적용
         </Button>
       </div>

3063a8621c48642e1f111d0b9366cda574db3a5f
Stems <kisn3089@naver.com>
2026-01-28 14:54:24 +0900

    api/util: validateEmptyObject 삭제

--- packages/api/utils/index.ts
+++ packages/api/utils/index.ts
@@ -1,2 +1,1 @@
 export * from "./priceFormatter";
-export * from "./validateEmptyObject";
--- packages/api/utils/validateEmptyObject.ts
+++
@@ -1,3 +0,0 @@
-export const validateEmptyObject = (obj: Record<string, unknown>): boolean => {
-  return Object.keys(obj).length === 0 && obj.constructor === Object;
-};

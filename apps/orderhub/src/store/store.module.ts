b51e32c01f75f7e8e55967fafb7dd1e73f0c2972
Stems <kisn3089@naver.com>
2026-02-03 00:12:43 +0900

    orderhub/fix: add version use store.module

--- apps/orderhub/src/store/store.module.ts
+++ apps/orderhub/src/store/store.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
+  imports: [RouterModule.register([{ path: 'store/v1', module: StoreModule }])],
   controllers: [StoreController],
   providers: [StoreService],
   exports: [StoreService],
